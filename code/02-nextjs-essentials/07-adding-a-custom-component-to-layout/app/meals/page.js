8056031fd47bd524ba8c538223f7df4975ada48d
Eugen Freund <Eugen.Freund@outlook.de>
2024-01-29 12:34:46 +0100

    Add custom component to MealsPage

--- code/02-nextjs-essentials/07-adding-a-custom-component-to-layout/app/meals/page.js
+++ code/02-nextjs-essentials/07-adding-a-custom-component-to-layout/app/meals/page.js
@@ -1,3 +1,28 @@
-export default function MealsPage() {
-  return <h1>Meals Page</h1>;
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+import { getAllMeals } from "@/lib/meals";
+
+export default async function MealsPage() {
+  const meals = await getAllMeals();
+
+  return <>
+    <header className={classes.header}>
+      <h1>
+        Delicious meals, created{' '}
+        <span className={classes.highlight}>by you</span>
+      </h1>
+      <p>
+        Choose your favorite meal and then easily
+        cook it yourself at home!
+      </p>
+      <p className={classes.cta}>
+        <Link href="/meals/share">
+          Share Your Favorite Recipe
+        </Link></p>
+    </header>
+    <main className={classes.main}>
+      <MealsGrid meals={meals} />
+    </main>
+  </>
 }
